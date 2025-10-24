46343c4931fe94fbe4274abe97fdfdf1c4d29539
siampathan005@gmail.com <siampathan005@gmail.com>
2025-10-24 23:40:20 +0600

    just change CORS code

--- backend-server/src/index.ts
+++ backend-server/src/index.ts
@@ -22,14 +22,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 connectDB();
 
-app.options("*", cors());
+app.use(cors({
+  origin: "https://sm-movie-admin.vercel.app/",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+} ));
 
-// app.use(cors({
-//   origin: 'http://localhost:5173/',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// } ));
 app.use(express.json());
 
 app.get("/", (req, res) => {
