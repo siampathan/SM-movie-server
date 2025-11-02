15e392435c2b70903de569f57d2b4f0d744162a0
siampathan005@gmail.com <siampathan005@gmail.com>
2025-11-02 13:52:47 +0600

    include mongoDB

--- backend-server/dist/index.js
+++ backend-server/dist/index.js
@@ -1,26 +1,17 @@
 "use strict";
-// import express, { Request, Response } from 'express'
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//   const app = express()
-//   const port = process.env.PORT || 8080
-//   app.get('/', (_req: Request, res: Response) => {
-//     return res.send('Welcome Siam In express server on Vercel!!')
-//   })
-//   app.get('/ping', (_req: Request, res: Response) => {
-//     return res.send('Ping - pong 🏓')
-//   })
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
-//import connectDB from './config/db';
-// import movieRouter from "./routes/movieRoutes";
-// import adminRouter from "./routes/adminRoutes";
+const db_1 = __importDefault(require("./config/db"));
+const movieRoutes_1 = __importDefault(require("./routes/movieRoutes"));
+const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8000;
-//connectDB();
+(0, db_1.default)();
 app.use((0, cors_1.default)({
     origin: "https://sm-movie-admin.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -34,8 +25,8 @@ app.get("/", (req, res) => {
     res.status(200).json({ messge: "Hello, Siam WelCome to ⚓ Server 😊!!" });
 });
 // Routes
-//app.use("/api/movies", movieRouter);
-//app.use("/api", adminRouter);
+app.use("/api/movies", movieRoutes_1.default);
+app.use("/api", adminRoutes_1.default);
 app.use((err, req, res, next) => {
     console.error("Error:", err);
     res.status(500).json({ error: err.message });
--- backend-server/src/index.ts
+++ backend-server/src/index.ts
@@ -1,26 +1,13 @@
-// import express, { Request, Response } from 'express'
-
-//   const app = express()
-//   const port = process.env.PORT || 8080
-
-//   app.get('/', (_req: Request, res: Response) => {
-//     return res.send('Welcome Siam In express server on Vercel!!')
-//   })
-
-//   app.get('/ping', (_req: Request, res: Response) => {
-//     return res.send('Ping - pong 🏓')
-//   })
-
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-//import connectDB from './config/db';
-// import movieRouter from "./routes/movieRoutes";
-// import adminRouter from "./routes/adminRoutes";
+import connectDB from './config/db';
+import movieRouter from "./routes/movieRoutes";
+import adminRouter from "./routes/adminRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-//connectDB();
+connectDB();
 
 app.use(cors({
   origin: "https://sm-movie-admin.vercel.app",
@@ -40,8 +27,8 @@ app.get("/", (req, res) => {
 });
 
 // Routes
- //app.use("/api/movies", movieRouter);
- //app.use("/api", adminRouter);
+ app.use("/api/movies", movieRouter);
+ app.use("/api", adminRouter);
 
  app.use((err:any, req:any, res:any, next:any) => {
   console.error("Error:", err);
