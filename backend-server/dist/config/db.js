579aeb044d13a01e4804b266019d202b9f844eb6
siampathan005@gmail.com <siampathan005@gmail.com>
2025-11-02 13:45:29 +0600

    include dist

--- backend-server/dist/config/db.js
+++ backend-server/dist/config/db.js
@@ -19,6 +19,7 @@ const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
         if (!dbUrl) {
             throw new Error("MONGODB_URL is not defined in the environment variables.");
         }
+        console.log("Database URL is:", dbUrl);
         mongoose_1.default.connection.on("connected", () => {
             console.log("DB Connected Successfully, Siam Boss don't worry 😊!!");
         });
@@ -30,35 +31,4 @@ const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 exports.default = connectDB;
-// import { MongoClient, ServerApiVersion } from "mongodb";
-// const uri = process.env.MONGODB_URL;
-// if (!uri) {
-//   throw new Error("❌ MONGODB_URL is not defined in environment variables.");
-// }
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// const connectDB = async (): Promise<void> => {
-//   try {
-//     // Connect to MongoDB cluster
-//     await client.connect();
-//     // Optional: ping the admin DB to confirm connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("✅ Pinged your deployment — successfully connected to MongoDB!");
-//   } catch (error) {
-//     console.error("❌ Database connection error:", error);
-//     process.exit(1);
-//   } finally {
-//     // You can choose to keep the connection open for the lifetime of the app
-//     // Comment out the following line if you want a persistent connection
-//     // await client.close();
-//   }
-// };
-// export default connectDB;
-// export { client };
 //# sourceMappingURL=db.js.map
--- backend-server/dist/index.js
+++ backend-server/dist/index.js
@@ -15,12 +15,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
-const db_1 = __importDefault(require("./config/db"));
-const movieRoutes_1 = __importDefault(require("./routes/movieRoutes"));
-const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
+//import connectDB from './config/db';
+// import movieRouter from "./routes/movieRoutes";
+// import adminRouter from "./routes/adminRoutes";
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8000;
-(0, db_1.default)();
+//connectDB();
 app.use((0, cors_1.default)({
     origin: "https://sm-movie-admin.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -34,8 +34,8 @@ app.get("/", (req, res) => {
     res.status(200).json({ messge: "Hello, Siam WelCome to ⚓ Server 😊!!" });
 });
 // Routes
-app.use("/api/movies", movieRoutes_1.default);
-app.use("/api", adminRoutes_1.default);
+//app.use("/api/movies", movieRouter);
+//app.use("/api", adminRouter);
 app.use((err, req, res, next) => {
     console.error("Error:", err);
     res.status(500).json({ error: err.message });
